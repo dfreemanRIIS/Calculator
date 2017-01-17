2bd15eff0203b1213cca1273f59fce7c3032ce8b
dfreemanRIIS <dfreeman@riis.com>
2017-01-17 14:58:46 -0500

    Add C and =.  Wire up #s

--- app/src/main/java/com/example/dfreeman/calculator/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dfreeman.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickZero(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "0");
+    }
+    public void onClickOne(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "1");
+    }
+    public void onClickTwo(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "2");
+    }
+    public void onClickThree(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "3");
+    }
+    public void onClickFour(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "4");
+    }
+    public void onClickFive(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "5");
+    }
+    public void onClickSix(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "6");
+    }
+    public void onClickSeven(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "7");
+    }
+    public void onClickEight(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "8");
+    }
+    public void onClickNine(View v) {
+        TextView firstField = (TextView)findViewById(R.id.firstInput);
+        firstField.setText(firstField.getText() + "9");
     }
 }
