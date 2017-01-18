ab6d4f7e0dead7cf32b2a3d2a7b2c42cb44b655d
dfreemanRIIS <dfreeman@riis.com>
2017-01-18 09:14:12 -0500

    Wire up first and second half operations

--- app/src/main/java/com/example/dfreeman/calculator/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/calculator/MainActivity.java
@@ -6,67 +6,101 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean secondHalf;
+    TextView firstField;
+    TextView secondField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        secondHalf = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstField = (TextView)findViewById(R.id.firstInput);
+        secondField = (TextView)findViewById(R.id.secondInput);
     }
 
     public void onClickZero(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "0");
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "0");
+        } else {
+            firstField.setText(firstField.getText() + "0");
+        }
     }
     public void onClickOne(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "1");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "1");
+        } else {
+            firstField.setText(firstField.getText() + "1");
+        }    }
     public void onClickTwo(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "2");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "2");
+        } else {
+            firstField.setText(firstField.getText() + "2");
+        }    }
     public void onClickThree(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "3");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "3");
+        } else {
+            firstField.setText(firstField.getText() + "3");
+        }    }
     public void onClickFour(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "4");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "4");
+        } else {
+            firstField.setText(firstField.getText() + "4");
+        }    }
     public void onClickFive(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "5");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "5");
+        } else {
+            firstField.setText(firstField.getText() + "5");
+        }    }
     public void onClickSix(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "6");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "6");
+        } else {
+            firstField.setText(firstField.getText() + "6");
+        }    }
     public void onClickSeven(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "7");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "7");
+        } else {
+            firstField.setText(firstField.getText() + "7");
+        }    }
     public void onClickEight(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "8");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "8");
+        } else {
+            firstField.setText(firstField.getText() + "8");
+        }    }
     public void onClickNine(View v) {
-        TextView firstField = (TextView)findViewById(R.id.firstInput);
-        firstField.setText(firstField.getText() + "9");
-    }
+        if(secondHalf){
+            secondField.setText(secondField.getText() + "9");
+        } else {
+            firstField.setText(firstField.getText() + "9");
+        }    }
     public void onPlusClick(View v) {
         TextView operatorField = (TextView)findViewById(R.id.operator);
         operatorField.setText("+");
+        secondHalf = true;
     }
     public void onMinusClick(View v) {
         TextView operatorField = (TextView)findViewById(R.id.operator);
         operatorField.setText("-");
+        secondHalf = true;
     }
     public void onMultClick(View v) {
         TextView operatorField = (TextView)findViewById(R.id.operator);
         operatorField.setText("X");
+        secondHalf = true;
     }
     public void onDivideClick(View v) {
         TextView operatorField = (TextView)findViewById(R.id.operator);
         operatorField.setText("%");
+        secondHalf = true;
+    }
+    public void onClickClear(View v) {
+        secondHalf = false;
     }
 }
